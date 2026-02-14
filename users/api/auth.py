d8956457b0d16b398cd9eae8ce665f1a3a162062
Noel George <noelgeorgejohn@gmail.com>
2026-02-14 17:14:58 -0500

    fix

--- users/api/auth.py
+++ users/api/auth.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, get_user_model
+from django.db.models import Q
 from ninja import Router
 
 from users.auth.jwt import create_access_token
@@ -10,25 +11,32 @@ User = get_user_model()
 router = Router()
 
 
-@router.post("/login", response=TokenOut)
+@router.post("/login", response={200: TokenOut, 401: TokenOut})
 def token(request, data: LoginIn):
     user = authenticate(request, username=data.username, password=data.password)
     if user is None:
-        return TokenOut(ok=False, error="Invalid credentials.")
+        return 401, TokenOut(ok=False, error="Invalid credentials.")
 
     token = create_access_token(user.id)
-    return TokenOut(ok=True, token=token, token_type="Bearer")
+    return 200, TokenOut(ok=True, token=token, token_type="Bearer")
 
 
-@router.post("/register", response=RegisterOut)
+@router.post("/register", response={200: RegisterOut, 400: RegisterOut})
 def register(request, data: RegisterIn):
     username = data.username
     email = data.email
-
-    user = User.objects.create_user(
-        username=username,
-        email=email,
-        password=data.password,
-        max_duration_chunk=data.max_duration_chunk or 60,
+    # check if account already exists with the same username or email
+    if User.objects.filter(Q(username=username) | Q(email=email)).exists():
+        return 400, RegisterOut(ok=False, error="Username or email already exists.")
+    try:
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=data.password,
+            max_duration_chunk=data.max_duration_chunk or 60,
+        )
+    except Exception as e:
+        return 400, RegisterOut(ok=False, error=str(e))
+    return 200, RegisterOut(
+        ok=True, id=user.id, username=user.username, email=user.email
     )
-    return RegisterOut(ok=True, id=user.id, username=user.username, email=user.email)
