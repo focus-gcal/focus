70fdf379bcd1f9689d10f87c5b82c924a04e1e3d
Your Anuj George <anujgeorge13@gmail.com>
2026-02-24 22:26:52 -0500

    Merge main into feature/schedule-template

--- backend/focus_gcal/api.py
+++ backend/focus_gcal/api.py
@@ -3,13 +3,12 @@ from schedules.api import api as schedules_api
 from tasks.api import api as tasks_api
 from time_slots.api import api as time_slots_api
 
-from users.api import api as users_api
+from users.api import api as auth_api
 
 api = NinjaAPI()
 
 # /users/ and /auth/ both serve the same router (auth + me) so clients work either way
-api.add_router("/users/", users_api.router)
-api.add_router("/auth/", users_api.router)
+api.add_router("/auth/", auth_api.router)
 api.add_router("/schedules/", schedules_api.router)
 api.add_router("/time_slots/", time_slots_api.router)
 api.add_router("/tasks/", tasks_api.router)
